630985ec714c80c352eae55bc61c124955b1e9e7
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-22 08:27:06 +0200

    updaet roles

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,8 +3,10 @@ export const Bio = {
   roles: [
     "Full Stack Developer",
     "Android Developer",
-    "UI/UX Designer",
+    "UI Designer",
     "Programmer",
+    "Apprentice",
+    "Student",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
