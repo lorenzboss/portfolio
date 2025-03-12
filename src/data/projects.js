35a37c295479db48d9d0f7801428e0cc9e0ee624
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-03-12 21:23:13 +0100

    Update project images.

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     date: "Jun 2024 - Jul 2024",
     description:
       "We created a web application inspired by Reddit's r/place event, where users can place pixels on a canvas. Unlike r/place, our app allows you to place a pixel every 10 seconds instead of waiting 5 minutes. Built with Next.js, the app leverages Supabase for real-time features and easy user authentication. ",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/r-place.png",
     tags: ["Next.js", "Supabase", "Tailwind CSS", "Node.js"],
     category: "web app",
     github: "https://github.com/BBZ-BL-IT/m426-project-r-place",
@@ -16,10 +16,21 @@ export const projects = [
     date: "Dec 2024",
     description:
       "Gym Tracker is a simple application to help you keep track of your workouts, monitor your progress, and achieve your fitness goals. It was built using Ionic and Capacitor, providing a seamless cross-platform experience for users.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/gym-tracker.png",
     tags: ["Angular", "Ionic", "Capacitor"],
     category: "mobile app",
     github: "https://github.com/lorenzboss/m335_gym-tracker",
+    member: [0],
+  },
+  {
+    title: "Pi Genius",
+    date: "Jan 2025 - Apr 2025",
+    description:
+      "I created a mobile app using React Native and Expo to make learning Pi both easy and enjoyable. The app emphasizes a clean, visually appealing UI and ensures an intuitive user experience, making the learning process more engaging and accessible.",
+    image: "/images/projects/pi-genius.png",
+    tags: ["React Native", "Expo"],
+    category: "mobile app",
+    github: "https://github.com/lorenzboss/pi-genius",
     member: [0],
   },
   {
@@ -27,7 +38,7 @@ export const projects = [
     date: "Apr 2024 - May 2024",
     description:
       "I developed a CRUD web application using Angular, Spring Boot, Java, and Keycloak. The app allows users to create podcasts, artists, and topics, while also providing user management and a role-based access control system.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/pod-controller.png",
     tags: ["TypeScript", "Angular", "Java", "Spring Boot", "Maven", "Keycloak"],
     category: "web app",
     github: "https://github.com/lorenzboss/m294_podcastmanager",
@@ -38,7 +49,7 @@ export const projects = [
     date: "Oct 2024",
     description:
       "This project analyzes data from 335 real estate sales in Basel-Landschaft (2011–2023). The logic was implemented both imperatively and functionally to compare the two approaches. Despite being a console app, it offers good user-friendliness and simple controls.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/properties-comparator.png",
     tags: ["Java", "JSON", "GSON", "Functional programming"],
     category: "application",
     github: "https://github.com/lorenzboss/m323_immobilien-vergleich",
@@ -49,29 +60,18 @@ export const projects = [
     date: "Dec 2024 - Jan 2025",
     description:
       "This project analyzes and compares the performance and memory usage of popular sorting algorithms in Python, including Bubble Sort, Quick Sort, and Cocktail Shaker Sort. It tests execution time on random lists and tracks memory usage during sorting. Additionally, We implemented multiple GitHub Actions and CI workflows for automated testing.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/python-algorithms-performance.png",
     tags: ["Python", "Github Actions", "CI"],
     category: "application",
     github: "https://github.com/lorenzboss/m450_LB3-algorithms-performance",
     member: [0, 1],
   },
   {
-    title: "Pi Genius",
-    date: "Jan 2025 - Apr 2025",
-    description:
-      "I created a mobile app using React Native and Expo to make learning Pi both easy and enjoyable. The app emphasizes a clean, visually appealing UI and ensures an intuitive user experience, making the learning process more engaging and accessible.",
-    image: "https://placehold.co/600x400",
-    tags: ["React Native", "Expo"],
-    category: "mobile app",
-    github: "https://github.com/lorenzboss/pi-genius",
-    member: [0],
-  },
-  {
     title: "Magnet Detector",
     date: "Apr 2024 - May 2024",
     description:
       "As part of the App Run (see experience), I developed a Magnet Detector app using Kotlin and Jetpack Compose. The app measures the current magnetic field and displays the readings in real time. Additionally, it features a QR code scanner, allowing users to easily scan and access information directly from codes.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/no-preview.png",
     tags: ["Kotlin", "Jetpack Compose", "Android APIs"],
     category: "mobile app",
     github: "https://github.com/lorenzboss/ar_magnetDetector",
@@ -82,7 +82,7 @@ export const projects = [
     date: "May 2024",
     description:
       "As part of the App Run (see experience), I developed a Treasure Map app using Kotlin and Jetpack Compose. The app allows users to view their current location on a map and place pins at specific spots. Users can later remove these pins and view the coordinates associated with each one. I integrated osmdroid and OpenStreetMap to build the map functionality, ensuring an intuitive and seamless user experience.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/no-preview.png",
     tags: ["Kotlin", "Jetpack Compose", "Android APIs", "OpenStreetMap"],
     category: "mobile app",
     github: "https://github.com/lorenzboss/ar_treasureMap",
@@ -93,7 +93,7 @@ export const projects = [
     date: "May 2024 - Jun 204",
     description:
       "As part of the App Run (see experience), I developed a Pixel Painter app using Kotlin and Jetpack Compose. The app allows users to draw individual pixels on a small canvas, creating custom images. Once the artwork is complete, users can send their creations to a server through a LogBook app, enabling easy sharing of their designs.",
-    image: "https://placehold.co/600x400",
+    image: "/images/projects/no-preview.png",
     tags: ["Kotlin", "Jetpack Compose"],
     category: "mobile app",
     github: "https://github.com/lorenzboss/ar_pixelPainter",
