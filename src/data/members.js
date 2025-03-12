4350e89011be92eff4575f675de11202b153d1b5
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-03-12 18:07:02 +0100

    Create new members.

---
+++ src/data/members.js
@@ -0,0 +1,37 @@
+export const members = [
+  {
+    id: 0,
+    name: "Lorenz Boss",
+    img: "https://media.licdn.com/dms/image/v2/D5603AQEZrwfLES9Kig/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718357360852?e=1747267200&v=beta&t=l4bAyLboJu8xPEsa-1JqoE7U9rB7eoe1TBHn0dokihA",
+    linkedin: "https://www.linkedin.com/in/lorenz-boss-229b06309/",
+    github: "https://github.com/lorenzboss",
+  },
+  {
+    id: 1,
+    name: "Leandro Aebi",
+    img: "https://media.licdn.com/dms/image/v2/D4E03AQHRJAkwnvNFdw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1682491452198?e=1747267200&v=beta&t=cbYnXJMSg7otSXp1mzrqq8bgGrgwugBmRGbifL_su2k",
+    linkedin: "https://www.linkedin.com/in/leandro-aebi-b3bbb521a/",
+    github: "https://github.com/leandroaebi",
+  },
+  {
+    id: 2,
+    name: "Levin Fankhauser",
+    img: "https://media.licdn.com/dms/image/v2/D5603AQE1CzG3_7os5w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1676381749867?e=1747267200&v=beta&t=LBuRKvAJXiGc-CqGaRL-uEWBWPffUY0uBsFxsBmsyNk",
+    linkedin: "https://www.linkedin.com/in/levin-fankhauser/",
+    github: "https://github.com/levin-fankhauser",
+  },
+  {
+    id: 3,
+    name: "Cem Akkaya",
+    img: "https://media.licdn.com/dms/image/v2/D4E03AQFfxw4vpA8YjA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1729997617324?e=1747267200&v=beta&t=nWmjgOnHuYIkFkb1IMkfKj9cxHra49up2rCrXGWZ4mk",
+    linkedin: "https://www.linkedin.com/in/cem-akkaya-78790a247/",
+    github: "https://github.com/cemmakkaya",
+  },
+  {
+    id: 4,
+    name: "Tobias Topp",
+    img: "https://avatars.githubusercontent.com/u/136596388?v=4",
+    linkedin: "https://www.linkedin.com/in/tobias-topp-95b27b281/",
+    github: "https://github.com/ToppTobi",
+  },
+];
