df2f0439ba09a84ad69d8253cf3be9713e586893
lorenz <142412373+lorenzboss@users.noreply.github.com>
2025-03-14 12:22:55 +0100

    Update bio.js

--- src/data/bio.js
+++ src/data/bio.js
@@ -9,7 +9,9 @@ export const Bio = {
     "a Student",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I live in Basel, Switzerland, and I am currently completing an apprenticeship as an IT specialist at Coop, the country's largest retailer. I am motivated, dedicated, and always ready to take on new challenges. I am passionate about learning and continuously improving myself, striving for excellence in everything I do. My interests include sports, technology, and computer science. In the future, I plan to expand my knowledge further and pursue a university degree.
+
+",
   resume:
     "https://drive.google.com/file/d/163gy82PJ-jFyOib5PKaeugoWXDu3O7ZI/view?usp=sharing",
   github: "https://github.com/lorenzboss",
