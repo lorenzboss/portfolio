45e17ae463cdcd85e3c02e0b399e6a78dde045d8
lorenz <lorenz.boss@bbzbl-it.ch>
2026-01-27 11:02:18 +0100

    Update HeroImage

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -137,7 +137,9 @@ const ContactMeButton = styled.div`
     hsla(271, 100%, 50%, 1) 0%,
     hsla(294, 100%, 50%, 1) 100%
   );
-  box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+  box-shadow:
+    20px 20px 60px #1f2634,
+    -20px -20px 60px #1f2634;
   border-radius: 50px;
   text-align: center;
   transition: all 0.4s ease-in-out;
@@ -221,7 +223,7 @@ const Hero = () => {
               <motion.div {...headContentAnimation}>
                 <Tilt>
                   <Img
-                    src="/HeroImage.webp"
+                    src="/HeroImage.avif"
                     alt="Lorenz Boss"
                     loading="eager"
                     fetchpriority="high"
